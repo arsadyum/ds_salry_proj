fd1328c9d47ce7b50b4ca955df7986a82f55e2a2
Arsad <arsad.ru.fin@gmail.com>
2020-05-20 01:54:27 -0400

    1000 rows were scrapped

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ds_salry_proj
+
+#May 20th the screapper was ran for 100o rows. 
--- main.py
+++ main.py
@@ -10,4 +10,5 @@ import pandas as pd
 
 path = "C:/Users/rahma/Documents/ds_salry_proj/chromedriver"
 
-df = gs.get_jobs('data engineer',15, False, path, 15) #this will scrap 20 fields 
+df = gs.get_jobs('data engineer',1000, False, path, 15) #this will scrap 1000 fields one change being made in the glassscrap.py line 56 as jobContainer
+
